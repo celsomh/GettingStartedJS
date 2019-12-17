1818058c71a07b64072ab5150ef6b675678720ee
celsomh <c.martin01@ufromail.cl>
2019-12-16 20:55:32 -0400

    First commit

---
+++ GettingStarted.js
@@ -0,0 +1,52 @@
+//Linea de comentario
+var miDato;
+miDato=5;
+
+var nombre;
+nombre="Juan";
+
+var _Nombre2;
+
+var $Nombre3;
+
+var numberVar;
+numberVar=1234;
+
+var booleanVar=false;
+
+var stringVar="Cadena de la variable stringVar";
+var stringVar='Nuevo valor de la variable stringVar pero en comillas simples';
+
+var undefiniedVar=undefinied;
+
+var suma=5+2;
+var resta=9-2;
+var divisiones=6/2;
+var multiplicacion=3*18;
+
+var cadenaUno="Hola ";
+var cadenaDos="Mundo!";
+var saludo=cadenaUno+cadenaDos;
+
+var x=1;
+var y=++x;
+var z=y++ + x;
+
+typeof saludo;
+typeof 10;
+typeof "Hi!";
+typeof undefinied;
+
+!booleanVar;
+!!booleanVar;
+
+true===true;     //true
+true===false;    //false
+true!==false;    //true
+true!==true;     //false
+
+5>3;     //true
+5<4;     //false
+10>=10;  //true
+15<=20;  //true
+
